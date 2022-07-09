f5b407453710fca0db15b5c441f6260cb33309af
bootun <bootunzhao@outlook.com>
2022-07-09 23:33:08 +0800

    feat: output easy to process format

--- cmd/veronica.go
+++ cmd/veronica.go
@@ -10,30 +10,20 @@ import (
 )
 
 var (
-	// 项目根目录
 	projectPath string
-
-	// 从标准输入读取文件
-	inputMode bool
 
 	changedFiles []string
 )
 
 func init() {
 	flag.StringVar(&projectPath, "path", ".", "project path")
-	flag.BoolVar(&inputMode, "input", false, "input from stdin")
 }
 
 // TODO: refactor
 func main() {
 	log.SetFlags(0)
 	flag.Parse()
-	
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		changedFiles = append(changedFiles, scanner.Text())
-	}
-	
+
 	project, err := parser.NewProject(projectPath)
 	if err != nil {
 		log.Fatal(err)
@@ -41,5 +31,16 @@ func main() {
 	if err := project.Parse(); err != nil {
 		log.Fatal(err)
 	}
-	project.ReportImpact(changedFiles)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		changedFiles = append(changedFiles, scanner.Text())
+	}
+	entrypoints, err := project.GetAffectedEntrypoint(changedFiles)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range entrypoints {
+		log.Printf("%s", v)
+	}
 }
