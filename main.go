8df5d2e71eed2ec7df8121f833b63ba9ce4f527a
bootun <bootunzhao@outlook.com>
2022-07-27 23:11:06 +0800

    feat: support version flag

--- main.go
+++ main.go
@@ -3,27 +3,39 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/bootun/veronica/parser"
 )
 
+const (
+	Version = "0.1.2"
+)
+
 var (
 	projectPath string
+
+	versionFlag bool
 
 	changedFiles []string
 )
 
 func init() {
 	flag.StringVar(&projectPath, "path", ".", "project path")
+	flag.BoolVar(&versionFlag, "version", false, "print version")
 }
 
 // TODO: refactor
 func main() {
 	log.SetFlags(0)
 	flag.Parse()
-
+	if versionFlag {
+		fmt.Printf("%s\n", Version)
+		os.Exit(0)
+	}
 	project, err := parser.NewProject(projectPath)
 	if err != nil {
 		log.Fatal(err)
