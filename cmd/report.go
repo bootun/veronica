dc3cbbbac34c6df848f2268bad0629402dc5497e
zhaoyihang <zhaoyihang@qimao.com>
2025-03-21 09:13:31 +0800

    feat: support custom commit

--- cmd/report.go
+++ cmd/report.go
@@ -12,21 +12,23 @@ import (
 
 var reportCmd = &cobra.Command{
 	Use:     "report",
-	Example: `  veronica report .`,
+	Example: `  veronica report --old HEAD~1 --new HEAD`,
 	Short:   "report the scope of impact of code changes",
-
 	Run: func(command *cobra.Command, args []string) {
-		if len(args) != 1 {
-			log.Fatal("Usage: veronica report <project-path>")
+		if oldCommit == "" {
+			log.Fatal("Usage: veronica report --old <old-commit> --new <new-commit>")
+		}
+		if newCommit == "" {
+			log.Fatal("Usage: veronica report --old <old-commit> --new <new-commit>")
 		}
 
-		cmd := exec.Command("git", "diff", "--name-only", "HEAD~1", "HEAD")
+		cmd := exec.Command("git", "diff", "--name-only", oldCommit, newCommit)
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			log.Fatalf("git diff: %s", output)
 		}
 
-		project, err := parser.NewProject(args[0])
+		project, err := parser.NewProject(repo)
 		if err != nil {
 			log.Fatalf("load project: %s", err)
 		}
@@ -53,6 +55,9 @@ var reportCmd = &cobra.Command{
 
 var (
 	outputFormat string
+	oldCommit    string
+	newCommit    string
+	repo         string
 )
 
 const (
@@ -62,4 +67,7 @@ const (
 
 func init() {
 	reportCmd.Flags().StringVarP(&outputFormat, "format", "f", OutputFormatOneLine, "output format, options: oneline, text")
+	reportCmd.Flags().StringVarP(&oldCommit, "old", "o", "", "old commit")
+	reportCmd.Flags().StringVarP(&newCommit, "new", "n", "", "new commit")
+	reportCmd.Flags().StringVarP(&repo, "repo", "r", ".", "repo path")
 }
